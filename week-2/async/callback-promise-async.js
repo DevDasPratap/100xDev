d2f018099424db6d70c250b47c53058be4dbfab7
devdaspratap <contact.pratap.das@gmail.com>
2023-12-26 22:46:38 +0530

    callback promise async

--- week-2/async/callback-promise-async.js
+++ week-2/async/callback-promise-async.js
@@ -47,7 +47,6 @@
 //   });
 // });
 
-
 // Promise
 
 // const promise = new Promise((resolve, reject)=>{
@@ -55,7 +54,6 @@
 //     console.log(Promise)
 // })
 // console.log(promise)
-
 
 // API promise
 // function getData(dataId, getNextData) {
@@ -78,18 +76,136 @@
 // }, 7000);
 
 // with then catch
-const getPromise = () =>{
-return new Promise((resolve, reject)=>{
-    console.log('Promise')
-    resolve('Success')
-    // reject('error')
-})
+// const getPromise = () =>{
+// return new Promise((resolve, reject)=>{
+//     console.log('Promise')
+//     resolve('Success')
+//     // reject('error')
+// })
+// }
+
+// const promise = getPromise()
+// promise.then((res)=>{
+//     console.log('Promise fullfield: ', res)
+// })
+// promise.catch((err)=>{
+//     console.log('Promise rejected: ', err)
+// })
+
+// callback with callback hell
+// function loadingData(callback) {
+//     setTimeout(()=>{
+//         console.log('1. Loading Data')
+//         callback()
+//     }, 4000)
+// }
+// function collectingData(callback) {
+//     setTimeout(()=>{
+//     console.log('2. Collecting Data')
+//     callback()
+// }, 1000)
+// }
+// function approvingData(callback) {
+//     setTimeout(()=>{
+//     console.log('3. Approving Data')
+//     callback()
+// }, 5000)
+// }
+// function approved() {
+//     console.log('4. Approved Data')
+// }
+
+// loadingData(function () {
+//     collectingData(function () {
+//         approvingData(function () {
+//             approved()
+//         })
+//     })
+// })
+
+// promise
+
+function loadingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("1. Loading Data");
+      resolve('load successfully');
+    }, 4000);
+  });
+}
+function collectingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("2. Collecting Data");
+      resolve('full fill');
+    }, 1000);
+  });
+}
+function approvingData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("3. Approving Data");
+      reject('not full fill');
+    }, 5000);
+  });
 }
 
-const promise = getPromise()
-promise.then((res)=>{
-    console.log('Promise fullfield: ', res)
+function approved() {
+  console.log("4. Approved Data");
+}
+
+// promise
+// loadingData()
+// .then(collectingData)
+// .then(approvingData)
+// .then(approved)
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+// async
+// async function asyncFn() {
+//     await loadingData()
+//     await collectingData()
+//     await approvingData()
+//     await approved()
+// }
+// asyncFn().catch((err)=>{
+//     console.log(err)
+// })
+
+// error handaling
+// try catch throw
+// function tct(a,b) {
+//     return a/b
+// }
+// console.log(tct(10, 0))
+// function tct(a,b) {
+//     try {
+//         if (b === 0) {
+//             throw new Error(`can't divide by 0`)
+//         }else{
+//             return a/b
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// console.log(tct(10, 5))
+
+// Higher order function
+const num = [10, 16, 22, 36, 90]
+const a = num.map((val)=>{
+    return val + 5
 })
-promise.catch((err)=>{
-    console.log('Promise rejected: ', err)
+console.log('Map: ', a) //new array
+console.log('Orginal: ',num) //orginal array
+
+const b = num.filter((val)=>{
+    if (val >= 10 && val <= 25) {
+        return val
+    }
 })
+console.log('Filter: ', b)
+console.log('Orginal: ',num) //orginal array
+
