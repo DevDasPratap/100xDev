986b07e42bccb043c623ff764bfa36c779467927
devdaspratap <contact.pratap.das@gmail.com>
2024-12-09 22:59:37 +0530

    postgreSQl

--- week-8/paytm/backend/config.js
+++ week-8/paytm/backend/config.js
@@ -1,5 +1,8 @@
 const config = {
     JWT_SECRECT: 'pratapdas',
+    DB_USER: 'payPD',
+    DB_PASS: 'yHfLWJJyFhrmyGmf',
+    DB_URL:'mongodb+srv://payPD:yHfLWJJyFhrmyGmf@cluster0.0t8sh.mongodb.net/'
 }
 
 module.exports = config
